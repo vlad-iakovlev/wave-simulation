9a3ac6309f71a4a6f092482d5349ec3afab597d0
Vladislav Yakovlev <vlad309523@gmail.com>
2022-12-04 23:56:30 +0400

    chore: codestyle

--- classes/FieldImage.ts
+++ classes/FieldImage.ts
@@ -3,12 +3,12 @@ const MASS_SHIFT = [0.02, 0, -0.04]
 type PixelValueSource = 'height' | 'accumulated'
 
 export class FieldImage {
-  pixelMass: number[][] = []
-  pixelHeight: number[][][] = []
-  pixelVelocity: number[][][] = []
-  pixelAccumulated: number[][][] = []
+  private pixelMass: number[][] = []
+  private pixelHeight: number[][][] = []
+  private pixelVelocity: number[][][] = []
+  private pixelAccumulated: number[][][] = []
 
-  constructor(public width: number, public height: number) {
+  constructor(private width: number, private height: number) {
     for (let x = 0; x < width; x++) {
       this.pixelMass[x] = []
       this.pixelHeight[x] = []
@@ -20,6 +20,26 @@ export class FieldImage {
         this.pixelHeight[x][y] = [0, 0, 0]
         this.pixelVelocity[x][y] = [0, 0, 0]
         this.pixelAccumulated[x][y] = [0, 0, 0]
+      }
+    }
+  }
+
+  setMass(condition: (x: number, y: number) => boolean, mass: number) {
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        if (condition(x, y)) {
+          this.pixelMass[x][y] = mass
+        }
+      }
+    }
+  }
+
+  setHeight(condition: (x: number, y: number) => boolean, height: number) {
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        if (condition(x, y)) {
+          this.pixelHeight[x][y] = [height, height, height]
+        }
       }
     }
   }
@@ -47,13 +67,13 @@ export class FieldImage {
             count += 1
           }
 
-          if (x < this.width - 1) {
-            force += this.pixelHeight[x + 1][y][i]
+          if (y > 0) {
+            force += this.pixelHeight[x][y - 1][i]
             count += 1
           }
 
-          if (y > 0) {
-            force += this.pixelHeight[x][y - 1][i]
+          if (x < this.width - 1) {
+            force += this.pixelHeight[x + 1][y][i]
             count += 1
           }
 
@@ -70,28 +90,8 @@ export class FieldImage {
     }
   }
 
-  setMass(condition: (x: number, y: number) => boolean, mass: number) {
-    for (let x = 0; x < this.width; x++) {
-      for (let y = 0; y < this.height; y++) {
-        if (condition(x, y)) {
-          this.pixelMass[x][y] = mass
-        }
-      }
-    }
-  }
-
-  setHeight(condition: (x: number, y: number) => boolean, height: number) {
-    for (let x = 0; x < this.width; x++) {
-      for (let y = 0; y < this.height; y++) {
-        if (condition(x, y)) {
-          this.pixelHeight[x][y] = [height, height, height]
-        }
-      }
-    }
-  }
-
-  getImageData(source: PixelValueSource) {
-    const image = new ImageData(this.width, this.height)
+  private getImageData(source: PixelValueSource) {
+    const imageData = new ImageData(this.width, this.height)
 
     for (let x = 0; x < this.width; x++) {
       for (let y = 0; y < this.height; y++) {
@@ -100,24 +100,24 @@ export class FieldImage {
           switch (source) {
             case 'height': {
               const height = this.pixelHeight[x][y][i]
-              image.data[index + i] =
+              imageData.data[index + i] =
                 (height < 0 ? 0 : height > 1 ? 1 : height) * 255
               break
             }
 
             case 'accumulated': {
               const accumulated = this.pixelAccumulated[x][y][i] * 0.0005
-              image.data[index + i] =
+              imageData.data[index + i] =
                 (accumulated > 1 ? 1 : accumulated * accumulated) * 255
               break
             }
           }
         }
-        image.data[index + 3] = 255
+        imageData.data[index + 3] = 255
       }
     }
 
-    return image
+    return imageData
   }
 
   draw(canvas: HTMLCanvasElement, source: PixelValueSource) {
@@ -125,7 +125,6 @@ export class FieldImage {
     canvas.height = this.height
 
     const ctx = canvas.getContext('2d')
-    ctx?.clearRect(0, 0, this.width, this.height)
     ctx?.putImageData(this.getImageData(source), 0, 0)
   }
 }
