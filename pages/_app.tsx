f6843fdba2517066a0c53199c26e0d9ddef5ab77
Vladislav Yakovlev <vlad309523@gmail.com>
2023-12-16 17:51:54 +0400

    fix: viewport params

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <NextHead>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover"
         />
       </NextHead>
 
